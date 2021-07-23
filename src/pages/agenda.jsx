27078c47ad6fd2e8694d7357fb595208339d308e
baucucu <alexandru.raduca@gmail.com>
2021-07-23 16:29:35 +0300

    calendar sorting

--- src/pages/agenda.jsx
+++ src/pages/agenda.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { Page, Navbar, Icon, Chip, CardHeader,CardContent, Card, CardFooter, Link, BlockTitle } from 'framework7-react';
+import { Button, Page, Navbar, Block, Chip, CardHeader,CardContent, Card, CardFooter, Link, BlockTitle } from 'framework7-react';
 var _ = require('lodash');
 var dayjs = require('dayjs')
 
@@ -19,23 +19,31 @@ const AgendaPage = (props) => {
       event.date = dayjs(event.fields["Start time"]).format("d MMM")
       return(event)
     }), 'date')
-    // console.log("days: ", tempDays)
-    setDays(tempDays)
+    let orderedDays = Object.keys(tempDays).sort().reduce(
+        (obj, key) => { 
+          obj[key] = tempDays[key]; 
+          return obj;
+        }, 
+        {}
+      );
+    console.log("days: ", orderedDays)
+    setDays(orderedDays)
   },[])
 
   return (
     <Page>
-      <Navbar title="Trevarefest Agenda"/>
-      {/* <!--
-      Additional "timeline-horizontal" className to enable Horizontal timeline
-      Additional "col-50" to define column width (50%)
-      Additional "tablet-20" to define column width for tablets (20%)
-      --> */}
-    <div className="timeline timeline-horizontal col-50 tablet-20">
-      {/* <!-- Timeline Item (Day) --> */}
-      
-      {days && Object.keys(days).map((date, id) => {return(<TimeLineDay key={id} date={date} events={days[date]}/>)})}
-    </div>
+          <Navbar title="Trevarefest Agenda"/>
+        {/* <!--
+        Additional "timeline-horizontal" className to enable Horizontal timeline
+        Additional "col-50" to define column width (50%)
+        Additional "tablet-20" to define column width for tablets (20%)
+        --> */}
+        <Block>Filters</Block>
+        <div className="timeline timeline-horizontal col-50 tablet-20">
+        {/* <!-- Timeline Item (Day) --> */}
+        
+        {days && Object.keys(days).map((date, id) => {return(<TimeLineDay key={id} date={date} events={days[date]}/>)})}
+        </div>
     </Page>
   );
 }
@@ -44,16 +52,19 @@ export default AgendaPage;
 
 const TimeLineDay = (props) => {
   const {date, events} = props;
+  
+  const orderedEvents = events.sort((a,b) => {return(new Date(a.fields["Start time"]) - new Date(b.fields["Start time"]))})
 
   useEffect(()=>{
     // console.log("day: ", date)
+    console.log("events: ", events)
   },[])
 
   return (
     <div className="timeline-item">
       <div className="timeline-item-date">{date}</div>
       <div className="timeline-item-content">
-        {events.map((event, id)=> {return(<TimeLineEvent key={id} event={event}/>)})}
+        {orderedEvents.map((event, id)=> {return(<TimeLineEvent key={id} event={event}/>)})}
       </div>
     </div>
   )
@@ -84,7 +95,7 @@ const TimeLineEvent = (props) => {
         {/* <p>{event.fields["Running order ID"]}</p> */}
       </CardContent>
       <CardFooter>
-        <Link color="white">Follow</Link>
+        <Button color="white">Follow</Button>
         {/* <Link>Read more</Link> */}
       </CardFooter>
     </Card>
